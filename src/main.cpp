2cd9698f610f1d2120dd8fa74cfec681355524cf
Bishop <bishop@tonietu.pl>
2024-09-03 22:04:26 +0200

    TFT_LCD with touchscreen

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,45 @@
 #include <Arduino.h>
+#include "TFT_eSPI.h"
 
-// put function declarations here:
-int myFunction(int, int);
+uint16_t x, y;
+bool color = true;
+unsigned long currentTime, nextTime;
+
+TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin( 115200 );
+  tft.init();
+  tft.setRotation( 3 );
+
+  uint16_t calData[5] = { 265, 3677, 261, 3552, 1 };
+  tft.setTouch(calData);
+
+  currentTime = nextTime = millis();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  currentTime = millis();
+  // tft.getTouchRaw(&x, &y);
+  bool touch = tft.getTouch(&x, &y);
+  if( touch ) {
+    Serial.printf("x: %i     ", x);
+    Serial.printf("y: %i     ", y);
+    Serial.printf("z: %i \n", tft.getTouchRawZ());
+  }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // handle stuff efvery 1 second
+  if( currentTime >= nextTime ) {
+    if( color ) {
+      tft.fillScreen( TFT_BLUE );
+    }
+    else {
+      tft.fillScreen( TFT_GREEN );
+    }
+
+    color = !color;
+    nextTime += 1000;
+
+    Serial.print( "." );
+  }
 }
