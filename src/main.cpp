9b039767e4c117d1cf5463c6cf5c290e7ca36c6f
Bishop <bishop@tonietu.pl>
2024-09-03 22:17:24 +0200

    Usefull code for touch calibration

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,57 @@ unsigned long currentTime, nextTime;
 
 TFT_eSPI tft = TFT_eSPI();
 
+// Code to run a screen calibration, not needed when calibration values are set in setup()
+void touch_calibrate()
+{
+  uint16_t calData[5];
+
+  // Calibrate
+  tft.fillScreen(TFT_BLACK);
+  tft.setCursor(20, 0);
+  tft.setTextFont(2);
+  tft.setTextSize(1);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+
+  tft.println("Touch corners as indicated");
+
+  tft.setTextFont(1);
+  tft.println();
+
+  tft.calibrateTouch(calData, TFT_MAGENTA, TFT_BLACK, 15);
+
+  Serial.println(); Serial.println();
+  Serial.println("// Use this calibration code in setup():");
+  Serial.print("  uint16_t calData[5] = ");
+  Serial.print("{ ");
+
+  for (uint8_t i = 0; i < 5; i++)
+  {
+    Serial.print(calData[i]);
+    if (i < 4) Serial.print(", ");
+  }
+
+  Serial.println(" };");
+  Serial.print("  tft.setTouch(calData);");
+  Serial.println(); Serial.println();
+
+  tft.fillScreen(TFT_BLACK);
+  
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.println("Calibration complete!");
+  tft.println("Calibration code sent to Serial port.");
+
+  delay(10000);
+}
+
 void setup() {
   Serial.begin( 115200 );
   tft.init();
   tft.setRotation( 3 );
 
-  uint16_t calData[5] = { 265, 3677, 261, 3552, 1 };
-  tft.setTouch(calData);
+  touch_calibrate();
+  // uint16_t calData[5] = { 265, 3677, 261, 3552, 1 };
+  // tft.setTouch(calData);
 
   currentTime = nextTime = millis();
 }
