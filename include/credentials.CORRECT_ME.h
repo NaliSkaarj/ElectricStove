81a04a4a73f645ec14294854494c2d557230b619
Bishop <bishop@tonietu.pl>
2024-10-15 21:48:11 +0200

    OTA and SerialToTelnet POC

---
+++ include/credentials.CORRECT_ME.h
@@ -0,0 +1,7 @@
+/*
+FIXME:
+- change this file name from "credentials.CORRECT_ME.h" to "credentials.h"
+- set proper SSID name and password to your WiFi network below
+*/
+const char *wifi_SSID = "*******";
+const char *wifi_PASS = "*******";
