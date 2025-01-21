e725191aa4f93f74e8d5d1ad1421df1f9a50dd55
Bishop <bishop@tonietu.pl>
2025-01-21 13:01:07 +0100

    SDCard write POC

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 #include "myOTA.h"
 #include "buzzer.h"
 #include "helper.h"
+#include "SD.h"
+#include "FS.h"
+
+#define SD_CS 34
 
 heater_state heaterState = STATE_IDLE;
 heater_state heaterStateRequested = STATE_IDLE;
@@ -11,6 +15,9 @@ unsigned long currentTime, next10S, next1S, next10mS, next100mS;
 static uint32_t targetHeatingTime;
 static uint16_t targetHeatingTemp;
 static xTimerHandle xTimer;
+
+String dataMessage;
+void writeFile(fs::FS &fs, const char * path, const char * message);
 
 static void updateTime( uint32_t time ) {
   targetHeatingTime = time;
@@ -81,6 +88,71 @@ void setup() {
   xTimerStart( xTimer, 5000 );
 
   currentTime = next10S = next1S = next10mS = millis();
+
+  // Initialize SD card
+  SD.begin( SD_CS );  
+  if( !SD.begin( SD_CS ) ) {
+    Serial.println( "Card Mount Failed" );
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+  if( cardType == CARD_NONE ) {
+    Serial.println( "No SD card attached" );
+    return;
+  }
+  // If the data.txt file doesn't exist
+  // Create a file on the SD card and write the data labels
+  File file = SD.open("/data.txt");
+  if(!file) {
+    Serial.println("File doens't exist");
+    Serial.println("Creating file...");
+    writeFile(SD, "/data.txt", "Reading ID, Date, Hour, Temperature \r\n");
+  }
+  else {
+    Serial.println("File already exists");  
+  }
+  file.close();
+}
+
+// Write to the SD card (DON'T MODIFY THIS FUNCTION)
+void writeFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if(!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
+}
+
+// Append data to the SD card (DON'T MODIFY THIS FUNCTION)
+void appendFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if(!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
+
+void logSDCard() {
+  dataMessage = String(12) + "," + String(34) + "," + String(56) + "," + String(78) + "\r\n";
+  Serial.print( "Save data: " );
+  Serial.println( dataMessage );
+  appendFile( SD, "/data.txt", dataMessage.c_str() );
 }
 
 void loop() {
@@ -126,6 +198,7 @@ void loop() {
   if( currentTime >= next10S ) {
     next10S += 10000;
     BUZZ_Add( 100 );
+    logSDCard();
   }
 
   switch( heaterState ) {
